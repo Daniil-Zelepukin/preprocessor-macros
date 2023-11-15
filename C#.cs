8cad52a157ee0b58a9b540507ef73d714c02acd5
Daniil-Zelepukin <121063291+Daniil-Zelepukin@users.noreply.github.com>
2023-11-15 20:46:42 +0300

    Add files via upload

---
+++ C#.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x, y;
+        Console.Write("Введите первое число: ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Максимальное число: " + Math.Max(x, y));
+
+        Console.WriteLine("-------------------------------");
+
+        int c, d;
+        Console.Write("Введите первое число: ");
+        c = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        d = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Максимальное число: " + Math.Min(c, d));
+
+        Console.WriteLine("-------------------------------");
+
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        Console.WriteLine("Площадь круга: " + area);
+
+        Console.WriteLine("-------------------------------");
+
+        Console.Write("x = ");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Число в квадрате: " + Math.Pow(x, 2));
+        Console.WriteLine("Число в 4-й степени: " + Math.Pow(x, 4));
+
+    }
+}
---
+++ C++.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+#define MAX(a, b) ( (a) > (b) ? (a) : (b) ) // макрос поиска максимального значения из 2 чисел
+#define MIN(a, b) ((a) < (b) ? (a) : (b)) // макрос поиска минимального значения из 2 чисел
+#define PI 3.14159 // поиск площади круга через константу PI
+#define SQR(x) ((x) * (x)) // возведение числа в квадрат
+#define POWER_4(x) (SQR(x) * SQR(x)) // возведение числа в 4 степень 
+
+int main() {
+    int x, y;
+    cout << "Введите первое число: ";
+    cin >> x;
+    cout << "Введите второе число: ";
+    cin >> y;
+    cout << "Максимальное число: " << MAX(x, y) << endl;
+
+    cout << "-------------------------------" << endl;
+
+    int c, d;
+    cout << "Введите первое число: ";
+    cin >> c;
+    cout << "Введите второе число: ";
+    cin >> d;
+    cout << "Минимальное число: " << MIN(c, d) << endl;
+
+    cout << "-------------------------------" << endl;
+
+    double radius = 5.0;
+    double area = PI * radius * radius;
+    cout << "Площадь круга: " << area << endl;
+
+    cout << "-------------------------------" << endl;
+
+    cout << "x = ";
+    cin >> x;
+    cout << "Число в квадрате  = " << SQR(x) << endl;
+    cout << "Число в 4 степени = " << POWER_4(x) << endl;
+
+    return 0;
+}
+
